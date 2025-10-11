6bb394fa90eddadb577b5b17cceab4e81f3b9975
Strowwberrys <bakharevaalina@gmail.com>
2025-10-11 12:46:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Setup Drosera Ciltrap..."
+echo "Setup Drosera Ciltrap..." 
 echo "Verifying Drosera configuration..."
 drosera verify --private-key YOUR_PRIVATE_KEY
 echo "Done."
